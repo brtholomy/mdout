8a6f62789a786ba654875fe0c725f6a7ea3110d2
bth <gh@hautogdoad.com>
2025-03-10 18:47:40 -0700

    use an associative array rather than run it manually

--- sed.sh
+++ sed.sh
@@ -1,12 +1,52 @@
 # run sed on pandoc .html output
 
+####################
+# replacements
+
+# key: SEDFROM, value: SEDTO
+typeset -A replacements=()
+
+# section
+replacements['<hr \/>']='<p>¢<\/p>'
+
+# EN dash
+# NOTE: pandoc inserts newlines in the html, this can break at the dash, such
+# that sed won't find it:
+replacements[' -[ ]']=' – '
+
+# blockquote source
+# I mark source in markdown with a double:
+# >>
+# NOTE: the literal \v : i think \v only works in sed within ""
+# NOTE: the extra  to prevent this from matching again:
+replacements['<blockquote><blockquote><p>']='<blockquote><blockquote><p>₱'
+
+# blockquote
+replacements['<blockquote><p>']='<blockquote><p>¥'
+
+# code block indent
+replacements['<pre><code><p>']='<pre><code><p>¥'
+
+# superscript
+replacements['<sup>']='<sup>¤'
+
+# footnote back anchor.
+# FIXME: don't know why this regex fails:
+# replacements['<a*.*? class=\"footnote-back\" role=\"doc-backlink\">↩︎<\/a>']=''
+replacements['↩︎']=''
+
+# h2
+replacements['<h2>']='<h2>£'
+
+####################
+# set up tmp files
+#
 # piping cat like this and then writing to the file will end up with an empty
 # file. Use a temp file instead.
-
+#
 # I do this rather than pipe the seds together, or do an inplace, so that I can
 # redefine the variables in between and get a result I can check before
 # replacing the source file.
-
 SEDIN='/tmp/in.html'
 SEDOUT='/tmp/out.html'
 cat $1 > $SEDIN
@@ -26,63 +66,13 @@ bth_runsed () {
 }
 
 ####################
-# replacements
+# run it
 
-# section
-SEDFROM='<hr \/>'
-SEDTO='<p>¢<\/p>'
-
-bth_runsed
-
-# EN dash
-# NOTE: pandoc inserts newlines in the html, this can break at the dash, such
-# that sed won't find it:
-SEDFROM=' -[ ]'
-SEDTO=' – '
-
-bth_runsed
-
-# blockquote source
-# I mark source in markdown with a double:
-# >>
-SEDFROM='<blockquote><blockquote><p>'
-# NOTE: the extra  to distinguish from single blockquote:
-SEDTO='<blockquote><blockquote><p>₱'
-
-bth_runsed
-
-# blockquote
-# note the literal \v : i think \v only works in sed within ""
-SEDFROM='<blockquote><p>'
-SEDTO='<blockquote><p>¥'
-
-bth_runsed
-
-# code block indent
-SEDFROM='<pre><code><p>'
-SEDTO='<pre><code><p>¥'
-
-bth_runsed
-
-# superscript
-SEDFROM='<sup>'
-SEDTO='<sup>¤'
-
-bth_runsed
-
-# footnote back anchor.
-# FIXME: don't know why this regex fails:
-# SEDFROM='<a*.*? class=\"footnote-back\" role=\"doc-backlink\">↩︎<\/a>'
-# SEDTO=''
-SEDFROM='↩︎'
-SEDTO=''
-
-bth_runsed
-
-SEDFROM='<h2>'
-SEDTO='<h2>£'
-
-bth_runsed
+for key in "${!replacements[@]}"; do
+    SEDFROM="$key"
+    SEDTO="${replacements[$key]}"
+    bth_runsed
+done
 
 ####################
 # output
